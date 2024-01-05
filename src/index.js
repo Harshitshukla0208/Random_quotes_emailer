878f39c3a3a06cc6f6788505b7ed93b871d3df65
Harshit Shukla <mrharshit2409@gmail.com>
2024-01-05 19:33:48 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
-const Quote = require('../quotes.json')
 const {sendEmail} = require('./service/email-service')
 const cron = require('node-cron');
-
 
 const startServer = () => {
     const app = express();
@@ -14,15 +12,13 @@ const startServer = () => {
     app.listen(PORT, ()=>{
         console.log(`Server started on ${PORT}`);
         
-        const random = Math.floor(Math.random() * Quote.length);
-        const response = Quote[random];
         cron.schedule('0 8 * * *', () => {
             sendEmail
             (
-                '<mail_From>@gmail.com',    // sender emailId
-                '<mail_To>@gmail.com',      // reciever emailId
-                'Quote Of the day',         // title
-                response.quote              // text
+                'shuklatesting02@gmail.com',       // sender emailId
+                'shuklatesting02@gmail.com',      // reciever emailId
+                'Quote Of the day'               // title
+            
             )
         })
     })
--- src/service/email-service.js
+++ src/service/email-service.js
@@ -1,10 +1,19 @@
 const sender = require('../config/email-config');
-const sendEmail = (mailFrom, mailTo, mailSubject, mailBody) => {
+const Quote = require('../../quotes.json')
+
+const random = Math.floor(Math.random() * Quote.length);
+const response = Quote[random];
+console.log(response);
+
+const sendEmail = (mailFrom, mailTo, mailSubject) => {
     sender.sendMail({
         from: mailFrom,
         to: mailTo,
         subject: mailSubject,
-        text: mailBody
+        html:`  <h1>Quote Of the day</h1>
+                <h2>${response.quote}</h2>
+                <h2>${response.author}</h2>
+            `
     });
 }
 module.exports = {sendEmail}
