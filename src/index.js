b37e78bf371c1f51eaff0e1ee1976032a732e86c
Harshit Shukla <mrharshit2409@gmail.com>
2024-01-05 19:02:02 +0530

    Setup a cron for 8:00 AM everyday

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
 const Quote = require('../quotes.json')
 const {sendEmail} = require('./service/email-service')
-//const cron = require('node-cron');
+const cron = require('node-cron');
 
 
 const startServer = () => {
@@ -11,32 +11,20 @@ const startServer = () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
-    app.get('/allquotes', (req, res) => {
-        res.send({'allquotes': Quote});
-    })
-
-    // app.get('/randomquote', (req, res) => {
-    //     const random = Math.floor(Math.random() * Quote.length);
-    //     res.send(Quote[random]);
-    // })
-
     app.listen(PORT, ()=>{
         console.log(`Server started on ${PORT}`);
         
         const random = Math.floor(Math.random() * Quote.length);
         const response = Quote[random];
-        // cron.schedule('*/0.5 * * * *', () => {
-            
-        // })
-        sendEmail
-        (
-            'shuklatesting02@gmail.com',
-            'shuklatesting02@gmail.com',
-            'This is a testing mail',
-            response
-        )
-
-        
+        cron.schedule('0 8 * * *', () => {
+            sendEmail
+            (
+                '<mail_From>@gmail.com',    // sender emailId
+                '<mail_To>@gmail.com',      // reciever emailId
+                'Quote Of the day',         // title
+                response.quote              // text
+            )
+        })
     })
 }
 
